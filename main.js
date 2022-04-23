822c5a0483d9686b6306e195373d8fb9ba7f2bd9
pralakk16 <pralakk16@gmail.com>
2022-04-23 18:39:43 -0500

    Correct Structure

--- main.js
+++ main.js
@@ -16,4 +16,16 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene, camera);
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshBasicMaterial({color: 0xffff00});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere)
+
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
