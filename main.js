9b22b78c6858bd52089b1eaa04c20a6abf687500
unknown <aaron.3alexander@gmail.com>
2022-04-23 19:44:40 -0500

    commit

--- main.js
+++ main.js
@@ -2,30 +2,50 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(-170, 170, 40);
+
+
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg")
 })
 
 
+
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial({color: 0xffff00});
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere)
+renderer.render(scene, camera);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 50, 0);
+
+const loader = new GLTFLoader();
+
+loader.load( './models/Earth/Earth.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error(error );
+
+} );
 
 function animate(){
   requestAnimationFrame(animate);
 
-  renderer.render(scene, camera);
+  controls.update();
+
+  renderer.render(scene, camera)
 }
 
 animate();
